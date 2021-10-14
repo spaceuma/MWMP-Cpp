d640cd3e3358b5216296078a2953fff5ec39154c
Gonzalo J. Paz Delgado <gonzalopd96@uma.es>
2021-10-14 13:22:58 +0200

    Create CMake for google unit tests

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
 sudo apt-get -y install libopencv-dev cmake
+sudo apt install libgtest-dev build-essential cmake
+
+cur=$PWD
+cd /usr/src/googletest
+sudo cmake .
+sudo cmake --build . --target install
+
+cd $cur
 rm -rf build
 mkdir build
 cd build
 cmake -Wno-dev ..
 cmake --build ./
+
+mv runUnitTests ../test/unit/

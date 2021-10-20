b855375db20faed8367cecfb957b83449d5e15ca
Gonzalo J. Paz Delgado <gonzalopd96@uma.es>
2021-10-20 17:08:00 +0200

    Add functions to get the initial and goal state and input vectors

--- src/StateSpaceModels.hpp
+++ src/StateSpaceModels.hpp
@@ -286,6 +286,30 @@ public:
     // Returns the safety distance to obstacles of the robot.
     double getSafetyDistance();
 
+    // Returns the state vector given the robot state.
+    Eigen::VectorXd getInitialStateVector(const std::vector<double> & robot_pose,
+                                          const std::vector<double> & arm_positions);
+
+    // Returns the state vector given the robot state.
+    bool getInitialStateVector(const std::vector<double> & robot_pose,
+                               const std::vector<double> & arm_positions,
+                               Eigen::VectorXd & x);
+
+    // Returns the goal state vector given de desired goal ee pose.
+    Eigen::VectorXd getGoalStateVector(const std::vector<double> & goal_ee_pose);
+
+    // Returns the goal state vector given de desired goal ee pose.
+    bool getGoalStateVector(const std::vector<double> & goal_ee_pose, Eigen::VectorXd & x);
+
+    // Returns the input vector given the robot inputs.
+    Eigen::VectorXd getInputVector(const std::vector<double> & arm_speeds,
+                                   const std::vector<double> & wheel_speeds);
+
+    // Returns the input vector given the robot inputs.
+    bool getInputVector(const std::vector<double> & arm_speeds,
+                        const std::vector<double> & wheel_speeds,
+                        Eigen::VectorXd & u);
+
     // Returns the linearized state space model matrix A.
     // Size of A: number_states x number_states
     bool getLinearizedMatrixA(const std::vector<double> & x,

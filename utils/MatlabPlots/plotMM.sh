02dfb0bf8f3e1f0e9abee50afcfcd629a2df7067
Gonzalo J. Paz Delgado <gonzalopd96@uma.es>
2021-11-15 11:52:09 +0100

    Include sample to be plotted for checking the evolution of the motion planner

--- utils/MatlabPlots/plotMM.sh
+++ utils/MatlabPlots/plotMM.sh
@@ -9,17 +9,19 @@ NC='\033[0m' # No Color
 helpFunction()
 {
     echo ""
-    echo "Usage: $0 -t type_motion_planning"
-    echo -e "\t-t Type of motion plan to be plotted (unconstrained, constrained, stepped)"
+    echo "Usage: $0 -t type_motion_planning -s sample"
+    echo -e "\t-t Type of motion plan to be plotted (unconstrained, constrained, stepped)."
+    echo -e "\t-s Iteration sample to be plotted. The final result will be plotted if not indicated."
     echo ""
     echo -e "${ORANGE}[WARNING] Be sure to run the script inside its directory!${NC}"
     echo ""
     exit 1 # Exit script after printing help
 }
-while getopts "t:" opt
+while getopts "t:s:" opt
 do
     case "$opt" in
         t ) type_motion_plan="$OPTARG" ;;
+        s ) sample="$OPTARG" ;;
 	? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
     esac
 done
@@ -31,32 +33,33 @@ then
     type_motion_plan="stepped"
 fi
 
+# In case the sample is empty, take it as the final one
+if [ -z "$sample" ]
+then
+    echo -e "${ORANGE}[WARNING] As no sample is indicated, by default the final result will be plotted${NC}";
+    sample=""
+else
+    sample=${sample}_
+fi
+
 # Begin script in case all parameters are correct
-unconstrained="unconstrained"
-constrained="constrained"
-stepped="stepped"
+echo Plotting the ${type_motion_plan} motion plan results
 
 ### Saving script directory ###
 path_to_utils_MatlabPlots=$PWD
 
+
 ### Move to the log directory ###
 cd ../../test/unit/results/
-if [ "$type_motion_plan" = "$unconstrained" ]
-then
-    cp ./unconstrained_planned_state.txt ./planned_state.txt
-    cp ./unconstrained_planned_control.txt ./planned_control.txt
-    echo Plotting the unconstrained motion plan results
-elif [ "$type_motion_plan" = "$constrained" ]
-then
-    cp ./constrained_planned_state.txt ./planned_state.txt
-    cp ./constrained_planned_control.txt ./planned_control.txt
-    echo Plotting the constrained motion plan results
-elif [ "$type_motion_plan" = "$stepped" ]
-then
-    cp ./stepped_planned_state.txt ./planned_state.txt
-    cp ./stepped_planned_control.txt ./planned_control.txt
-    echo Plotting the stepped motion plan results
-fi
+
+### Generate paths for the log files ###
+full_path_state="${sample}${type_motion_plan}_planned_state.txt"
+full_path_control="${sample}${type_motion_plan}_planned_control.txt"
+
+echo Extracting logs from ${full_path_state} and ${full_path_control}
+
+cp $full_path_state ./planned_state.txt
+cp $full_path_control ./planned_control.txt
 
 cd $path_to_utils_MatlabPlots
 ../../../../MATLAB/R2021b/bin/matlab -nosplash -nodesktop -r "run('./plotMM.m');"
